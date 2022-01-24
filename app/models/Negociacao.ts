b17e9b8770e033bd05a676ebb9a50e82a3e59b12
pessolatohenrique <pessolatohenrique@gmail.com>
2022-01-24 20:41:29 -0300

    Utilizacao de programacao defensiva para impedir modificacao de data

--- app/models/Negociacao.ts
+++ app/models/Negociacao.ts
@@ -1,11 +1,18 @@
 export class Negociacao {
   constructor(
-    public readonly _data: Date,
+    private _data: Date,
     public readonly _valor: number,
     public readonly _quantidade: number
-  ) {}
+  ) {
+    this._data = _data;
+  }
 
   get volume() {
     return this._quantidade * this._valor;
+  }
+
+  get data(): Date {
+    const newDate = new Date(this._data.getTime());
+    return newDate;
   }
 }
--- dist/js/models/Negociacao.js
+++ dist/js/models/Negociacao.js
@@ -3,8 +3,13 @@ export class Negociacao {
         this._data = _data;
         this._valor = _valor;
         this._quantidade = _quantidade;
+        this._data = _data;
     }
     get volume() {
         return this._quantidade * this._valor;
+    }
+    get data() {
+        const newDate = new Date(this._data.getTime());
+        return newDate;
     }
 }
